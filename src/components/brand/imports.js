e37223ca4e98f3d309ef6df7906b800c8e7c428a
Persona743 <siyavashshahidi7@gmail.com>
2023-03-15 22:41:41 +0330

    brand added

---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from '../../../public/assets/image/google.png';
+import slack from '../../../public/assets/image/slack.png';
+import atlassian from '../../../public/assets/image/atlassian.png';
+import dropbox from '../../../public/assets/image/dropbox.png';
+import shopify from '../../../public/assets/image/shopify.png';
+
+export { google, slack, atlassian, dropbox, shopify };
